609da70260afe92768c838d37b67150e07d43298
Berke Sayın <sayinberke34@gmail.com>
2025-01-13 18:22:18 +0300

    Initialize application

---
+++ src/main/java/dev/berke/token_api/TokenApiApplication.java
@@ -0,0 +1,13 @@
+package dev.berke.token_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenApiApplication.class, args);
+	}
+
+}
