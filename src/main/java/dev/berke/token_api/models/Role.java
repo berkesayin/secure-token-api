52ae134d5cdd6265a79f1f2324f93ec230b22218
Berke Sayın <sayinberke34@gmail.com>
2025-02-04 17:20:44 +0300

    Add models

---
+++ src/main/java/dev/berke/token_api/models/Role.java
@@ -0,0 +1,27 @@
+package dev.berke.token_api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleType name;
+}
---
+++ src/main/java/dev/berke/token_api/models/RoleType.java
@@ -0,0 +1,7 @@
+package dev.berke.token_api.models;
+
+public enum RoleType {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
